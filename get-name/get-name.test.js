36147060e6150847ddb633567d8269910f983846
Eion Nelson <ecnelson1@live.com>
2021-03-22 13:51:20 -0700

    added get name folder and files, wrote failing getName test

---
+++ get-name/get-name.test.js
@@ -0,0 +1,7 @@
+describe('getName function', () => {
+  it('returns the name property of an object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const objectName = getName(spot);
+    expect(objectName).toEqual('spot');
+  });
+});
